ba86eadd8f2d0b45a74a663306247cdade7aff87
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-11-04 02:22:38 +0100

    🚀⚡

--- filesystem.js
+++ filesystem.js
@@ -1,2 +1,92 @@
-const fs = require('fs');
-fs.writeFile('file.txt', "sample text", (err)=> err? console.log(err): console.log('file successfully created'))
+const fs = require("fs");
+// fs.writeFile("file.txt", "sample text", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file created successfully");
+//   }
+// });
+
+// fs.readFile("file.txt", "utf8", (err, file) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(file);
+//   }
+// });
+
+// fs.rename("file.txt", "file2.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("successfully renamed");
+//   }
+// });
+
+// fs.appendFile("file2.txt", " This is the new content", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("successfully modified file");
+//   }
+// });
+
+// fs.unlink('file2.txt', (err)=>{
+//   if(err){
+//     console.log(err)
+//   } else{
+//     console.log(`Deleted ssuccessfully`)
+//   }
+// })
+
+// fs.mkdir("tutorial", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     fs.writeFile("./tutorial/example.txt", "automation", (err) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("file created successfully");
+//       }
+//     });
+//   }
+// });
+
+// fs.unlink("./tutorial/example.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     fs.rmdir("tutorial", (err) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("Folder removed successfully");
+//       }
+//     });
+//   }
+// });
+
+// fs.readdir("./tutorial", (err, files) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     for (let file of files) {
+//       fs.unlink(`./tutorial/${file}`, (err) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(`${file} successfully deleted`);
+//         }
+//       });
+//     }
+//   }
+// });
+
+const readStream = fs.createReadStream("./example.txt", "utf8");
+const writeStream = fs.createWriteStream("example2.txt");
+readStream.on("data", (chunk) => {
+  writeStream.write(chunk);
+});
+
+//readtsream uses a small buffer and this makes better than a readFile
